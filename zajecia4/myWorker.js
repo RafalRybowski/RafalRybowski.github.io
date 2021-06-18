4165beeb49e0e2779f410cab1b632066e9a6e93e
rafal <rafalrybowski1996@gmail.com>
2021-06-18 13:24:38 +0200

    add download

--- zajecia4/myWorker.js
+++ zajecia4/myWorker.js
@@ -11,9 +11,9 @@ this.xhttp = new XMLHttpRequest();
 this.xhttp.withCredentials = false;
 
 self.onmessage = (e) => {
-    setHttpResponse(e.data[0])
-    this.xhttp.open('GET', e.data[1])
-    this.xhttp.send()
+        setHttpResponse(e.data[0])
+        this.xhttp.open('GET', e.data[1], true)
+        this.xhttp.send()
   };
 
   function setHttpResponse(type) {
@@ -23,21 +23,47 @@ self.onmessage = (e) => {
                 if (this.readyState == 4 && this.status == 200) {
                     var res = JSON.parse(this.response)
                     if(res == this.loginError) {
-                        self.postMessage(false);
+                        self.postMessage(-1);
                     } else {
-                        self.postMessage(true);
+                        self.postMessage(res.LOGIN_OK);
                     }
                 }
             }
             break;
         }
         case types.register: {
+            this.xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var res = JSON.parse(this.response)
+                    if(res == this.registerError) {
+                        self.postMessage(false);
+                    } else {
+                        self.postMessage(true);
+                    }
+                }
+            };
             break;
         }
         case types.dow: {
+            this.xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var res = JSON.parse(this.response)
+                    var list = []
+                    res.forEach(function(item, index, array) {
+                      list.push(JSON.parse(item.scores))
+                    })
+                    self.postMessage(list);
+                }
+            };
             break;
         }
         case types.up: {
+            this.xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var res = JSON.parse(this.response)
+                    self.postMessage(res);
+                }
+            };
             break;
         }
     }
